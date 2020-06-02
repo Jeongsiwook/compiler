13bf04e52e715747ee3c9fc81d9908759324d4bd
Yerang Lim <51330260+limyer@users.noreply.github.com>
2020-06-02 16:55:04 +0900

    Update State.java

---
+++ CompilerProject/src/State.java
@@ -0,0 +1,36 @@
+package interpreter;
+
+import java.util.*;
+
+import syntax.expression.Variable;
+import syntax.value.Value;
+
+
+
+@SuppressWarnings("serial")
+public class State extends HashMap<Variable, Value> {
+	public State() {
+		super();
+	}
+
+	public State(Variable variable, Value val) {
+		super();
+		this.put(variable, val);
+	}
+
+	public State onion(Variable variable, Value val) {
+		this.put(variable, val);
+		return this;
+	}
+
+	public State onion(State other) {
+		for (Variable key : other.keySet())
+			put(key, other.get(key));
+		return this;
+	}
+	
+	public void display(){
+		for(Variable v : this.keySet())
+			System.out.println(v + ": " + this.get(v));
+	}
+}
